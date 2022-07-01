334c0cfa2f6c070dfd263bf9f31c29151ef40ce0
Soham Chakraborty <soham20chakraborty@gmail.com>
2022-07-01 16:01:24 +0530

    updates

--- Page&Script/myScript.js
+++ Page&Script/myScript.js
@@ -1,1 +1,25 @@
-console.log("Script running!");
+function passwordverification(){
+   var pwd = document.getElementById("password").value;
+   var cpwd = document.getElementById("passwordConfirm").value;
+   if(pwd ==""){
+    alert("Password cannot be empty");
+    return false;
+   }
+   if(pwd.length < 8) {  
+    alert("Password length must be atleast 8 characters"); 
+    return false;  
+ }  
+ 
+
+ if(pwd.length > 15) {    
+    alert("password length cannot be more than 15 characters")
+    return false;  
+ } else {
+    if(cpwd==pwd){  
+    alert("Password is valid");
+    }
+    else{
+        alert("Password & Confirm Password does not match");
+    }  
+ }  
+}  
